bb83236e6082eb1a4531c61929670a6ab04498c9
Enquil <jim.e.olesen@gmail.com>
2023-07-31 21:25:26 +0000

    added test for product.save method

--- products/test_models.py
+++ products/test_models.py
@@ -51,3 +51,63 @@ class TestCategoryModel(TestCase):
         self.assertEqual(
             Category.get_friendly_name(test_category), 'Test Category'
         )
+
+
+class TestProductModel(TestCase):
+
+    def setUp(self):
+
+        test_domain = Domain.objects.create(
+            name='test_domain',
+            friendly_name='Test Domain',
+        )
+        test_domain.save()
+
+        test_brand = Brand.objects.create(
+            name='test_brand',
+            friendly_name='Test Brand'
+        )
+
+        test_category = Category.objects.create(
+            name='test_category',
+            friendly_name='Test Category',
+            domain=test_domain,
+        )
+        test_category.save()
+
+    def test_product_save_method(self):
+
+        test_brand = Brand.objects.get(id=1)
+        test_category = Category.objects.get(id=1)
+
+        test_product = Product.objects.create(
+            name='test product',
+            brand=test_brand,
+            category=test_category,
+            description='test description',
+            has_sizes=False,
+            price=99.99,
+            rating=4,
+            m_or_f='U',
+        )
+        test_product.save()
+        print(test_product.sku)
+
+        # assert domain got auto-filled
+        self.assertEqual(
+            test_product.domain, 1
+        )
+
+        # Store last 5 digits of product.sku since they are random
+        sku_rand_int = (
+            test_product.sku[-5:-1] + test_product.sku[-1]
+        )
+
+        # assert test_product.sku returns 111 + sku_rand_int val
+        self.assertEqual(
+            test_product.sku,
+            f'{test_product.domain}' +
+            f'{test_product.brand.id}' +
+            f'{test_product.category.id}' +
+            sku_rand_int
+        )
