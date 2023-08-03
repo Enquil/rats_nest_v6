532b9aa473e758210135ad98879d8545a0d3c5b7
Enquil <jim.e.olesen@gmail.com>
2023-08-03 10:45:43 +0000

    formatted code

--- checkout/models.py
+++ checkout/models.py
@@ -76,8 +76,9 @@ class Order(models.Model):
             self.lineitems.aggregate
             (Sum('lineitem_total'))['lineitem_total__sum']) or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = (self.order_total *
-                                  settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+            self.delivery_cost = (
+                settings.STANDARD_DELIVERY_FEE
+            )
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
@@ -98,16 +99,24 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
 
-    order = models.ForeignKey(Order, null=False, blank=False,
-                              on_delete=models.CASCADE,
-                              related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False,
-                                on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
-                                         null=False, blank=False,
-                                         editable=False)
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems'
+    )
+    product = models.ForeignKey(
+        Product, null=False, blank=False,
+        on_delete=models.CASCADE
+    )
+    product_size = models.CharField(
+        max_length=2, null=True, blank=True
+    )
+    quantity = models.IntegerField(
+        null=False, blank=False, default=0
+    )
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2,
+        null=False, blank=False, editable=False
+    )
 
     def save(self, *args, **kwargs):
         self.lineitem_total = self.product.price * self.quantity
